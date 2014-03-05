58c9b160e0baf4c5f5812a51bb6cc95c003bea08
kelliwarren <kmdesigngroup@gmail.com>
2014-03-04 16:12:04 -0800

    made host change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ Facespace::Application.configure do
 
   config.cache_classes = false
   config.eager_load = false
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'http://radiant-dawn-9141.herokuapp.com' }
   config.action_mailer.delivery_method = :letter_opener
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
